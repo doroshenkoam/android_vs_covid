10c76eb5562d2284ceb84408bedceb507b476e7b
Алексей Дорошенко <doroshenkoam@altarixru>
2020-09-15 00:07:06 +0300

    [+] added enemy

--- main.py
+++ main.py
@@ -1,62 +1,119 @@
 import pygame
+from random import randint
 
 # инициализируем пакет
 pygame.init()
 # TODO: вынести в конфиг
 FPS = 60
-GAME_NAME = "SURVIVAL"
+GAME_NAME = "ANDROID VS COVID-19"
 WINDOW_SIZE = (800, 600)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
-BG = pygame.image.load("asset/priroda-nebo-oblaka-svezhest.jpg")
-MAN = pygame.image.load("asset/player.png")
+BG = pygame.image.load("asset/imgonline-com-ua-Resize-D1KIqVCWF3.jpg")
 WIN = pygame.display.set_mode(WINDOW_SIZE)
+runing = True
+
+
+def game_over():
+    global runing
+    runing = False
 
 
 # player - класс основного персонажа
-class player(object):
+class player(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("asset/player.png").convert_alpha()
+        self.rect = self.image.get_rect(center=(x, y))
         self.x = x
         self.y = y
         self.width = width
         self.height = height
-        self.velocity = 5
+        self.velocity = 10
         self.is_jump = False
         self.left = False
         self.right = False
         self.walk_count = 0
         self.jump_count = 10
+        self.health = 100
 
     def draw(self):
-        # print(MAN)
         # MAN 110x130*32
-        WIN.blit(MAN, (self.x, self.y))
+        WIN.blit(self.image, (self.x, self.y))
+
+    def wound(self):
+        self.health -= 10
+        print(self.health)
+        if self.health <= 0:
+            game_over()
 
 
-def re_draw_window(man):
+class enemy(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height, group):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("asset/virus.png").convert_alpha()
+        self.rect = self.image.get_rect(center=(x, y))
+        self.add(group)
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.velocity = 10
+        self.counter = 0
+
+    def draw(self):
+        WIN.blit(self.image, (self.x, self.y))
+
+    def move(self):
+        if self.x < 0:
+            self.x = 900 + randint(0, 300)
+        self.x -= self.velocity
+        self.counter += 1
+
+        # увеличиваем скорость
+        if self.counter == 10 * FPS:
+            self.velocity += 1
+            self.counter = 0
+
+
+def re_draw_window(man, virus):
     WIN.blit(BG, (0, 0))
     man.draw()
+    virus.draw()
     pygame.display.update()
 
 
 def main():
+    global runing
+
     # настройки окна
     pygame.display.set_caption(GAME_NAME)
 
     clock = pygame.time.Clock()
 
+    # игровой персонаж
+    man = player((800 - 130) / 2, 375, 110, 130)
+    # враги
+    viruses = pygame.sprite.Group()
+    virus = enemy(900, 375, 200, 200, viruses)
+    viruses.add(virus)
+
     # основной цикл
-    # TODO: вынести в конфиг
-    man = player((800 - 130) / 2, 450, 110, 130)
-    runinig = True
-    while runinig:
+    while runing:
 
         clock.tick(FPS)
 
         for i in pygame.event.get():
             if i.type == pygame.QUIT:
-                runinig = False
+                runing = False
 
+        # TODO: не работает
+        # враги
+        virus.move()
+        if pygame.sprite.spritecollide(man, viruses, True):
+            man.wound()
+
+        # управление
         keys = pygame.key.get_pressed()
 
         # анализ движения
@@ -91,7 +148,7 @@ def main():
                 man.is_jump = False
                 man.jump_count = 10
 
-        re_draw_window(man)
+        re_draw_window(man, virus)
 
 
 if __name__ == "__main__":
